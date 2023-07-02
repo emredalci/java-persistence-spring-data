9f76c97e78475556c30768fc1bf3f595710d22f0
emredalci <emredalc@gmail.com>
2023-07-02 15:21:56 +0300

    Added persistence context retrieve persistent

--- ManagingData/managing-data/src/test/java/com/example/SimpleTransitionsTest.java
+++ ManagingData/managing-data/src/test/java/com/example/SimpleTransitionsTest.java
@@ -6,7 +6,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class SimpleTransitionsTest {
 
@@ -14,7 +16,7 @@ class SimpleTransitionsTest {
             Persistence.createEntityManagerFactory("managing_data");
 
     @Test
-    void makePersistent(){
+    void makePersistent() {
         EntityManager em = emf.createEntityManager(); // Application-managed
 
         em.getTransaction().begin();
@@ -34,5 +36,52 @@ class SimpleTransitionsTest {
         em.close();
     }
 
+    @Test
+    void retrievePersistent() {
+        EntityManager em = emf.createEntityManager(); // Application-managed
+        em.getTransaction().begin();
+        Item item = new Item();
+        item.setName("Some item");
+        em.persist(item);
+        em.getTransaction().commit();
+        em.close();
+        Long itemId = item.getId();
+        assertEquals(1, itemId);
+
+        {
+            em = emf.createEntityManager();
+            em.getTransaction().begin();
+            Item retrievedItem = em.find(Item.class, itemId);// Hit the database if not already in persistence context
+            if (Objects.nonNull(retrievedItem)){
+                retrievedItem.setName("New Name");
+            }
+            em.getTransaction().commit(); // Flush: Dirty check and SQL UPDATE
+            em.close();
+            assertTrue(Objects.nonNull(retrievedItem));
+        }
+
+        {
+            em = emf.createEntityManager();
+            em.getTransaction().begin();
+
+            Item item1 = em.find(Item.class, itemId);
+            Item item2 = em.find(Item.class, itemId); // Repeatable read, Acts as first-level cache
+
+            assertSame(item1, item2);
+            assertTrue(item1.equals(item2));
+            assertTrue(item1.getId().equals(item2.getId()));
+
+            em.getTransaction().commit();
+            em.close();
+        }
+
+        {
+            em = emf.createEntityManager();
+            em.getTransaction().begin();
+            assertEquals("New Name", em.find(Item.class, itemId).getName());
+            em.getTransaction().commit();
+            em.close();
+        }
+    }
 
 }
